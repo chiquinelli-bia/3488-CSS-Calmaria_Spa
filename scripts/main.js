63cd6ceb7a48cafbf2a478f73a0b0dc849ed3a95
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-07-31 16:07:57 -0300

    implementar função para abrir e fechar submenu

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+function alternarSubmenu(item, mostrar) {
+  const subMenu = item.querySelector(".submenu");
+  if (subMenu) {
+    subMenu.style.display = mostrar ? "block" : "none";
+  }
+}
+
 document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
   item.addEventListener("mouseover", () => alternarSubmenu(item, true));
   item.addEventListener("mouseout", () => alternarSubmenu(item, false));
