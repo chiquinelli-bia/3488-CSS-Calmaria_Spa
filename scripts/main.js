02aeee9917506724a1a15eefc0d95cc8331024f1
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-07-31 17:38:08 -0300

    implementar acessibilidade e feeedback visual a função alternarSubmenu.
    através do aria expanded e rotação dinamica do icone do submenu

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@ function alternarSubmenu(item, mostrar) {
   const subMenu = item.querySelector(".submenu");
   if (subMenu) {
     subMenu.style.display = mostrar ? "block" : "none";
+    const menuItem = item.querySelector(".cabecalho__lista-item a");
+    menuItem.setAttribute("aria-expanded", mostrar ? true : false);
+    const SubMenuIcon = item.querySelector(".material-symbols-outlined");
+    SubMenuIcon.classList.toggle("active", mostrar);
   }
 }
 
@@ -10,7 +14,7 @@ document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
   item.addEventListener("mouseout", () => alternarSubmenu(item, false));
   item.addEventListener("click", () => {
     const subMenu = item.querySelector(".submenu");
-    const isDisplayed = subMenu.computedStyleMap.display === "block";
+    const isDisplayed = subMenu.style.display === "block";
     alternarSubmenu(item, !isDisplayed);
   });
 });
