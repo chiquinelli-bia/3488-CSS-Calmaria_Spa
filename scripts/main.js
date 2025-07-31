0d372814b6181094234f192fb0c6aa6700389cd9
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-07-31 15:59:20 -0300

    adicionar ouvinte ao clique do submenu

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
   item.addEventListener("mouseover", () => alternarSubmenu(item, true));
   item.addEventListener("mouseout", () => alternarSubmenu(item, false));
+  item.addEventListener("click", () => {
+    const subMenu = item.querySelector(".submenu");
+    const isDisplayed = subMenu.computedStyleMap.display === "block";
+    alternarSubmenu(item, !isDisplayed);
+  });
 });
